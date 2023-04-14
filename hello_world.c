8ba2cc8aec13783addd90333316ffa94b34eefc1
jgabaut <109908086+jgabaut@users.noreply.github.com>
2023-04-14 09:32:50 +0200

    1.0.0 Introduced git mode (#3)

---
+++ hello_world.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <dirent.h>
+
+int main(void) {
+  printf("Hello, World!\n");
+  printf("Built using make.\n");
+  printf("Amboso v1.0.0.\n");
+  return 0;
+}
