b77a91088341f6bdc596641124ffcabff9cb655e
jgabaut <109908086+jgabaut@users.noreply.github.com>
2023-05-06 02:36:26 +0200

    feat: 1.3.0 proper test mode

--- hello_world.c
+++ hello_world.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 #include <unistd.h>
-#include <dirent.h>
 
 int main(void) {
   printf("Hello, World!\n");
-  printf("Built using make.\n");
-  printf("Amboso v1.2.0, now with test mode!\n");
+  printf("Built using make and anvil around it.\n");
+  printf("Amboso v1.3.0, with better test mode!\n");
   return 0;
 }
