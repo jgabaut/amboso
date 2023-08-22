c4c7bc847a06ac732c5ba90c9ef83d15a60283e6
jgabaut <109908086+jgabaut@users.noreply.github.com>
2023-08-22 18:17:36 +0200

    feat: gen AMBOSO__API_LEVEL (#38)

--- example-src/hello_world.c
+++ example-src/hello_world.c
@@ -4,6 +4,7 @@
 
 int main(void) {
   printf("Hello, World!\n");
+  printf("Built using amboso API v%s\n",get_ANVIL__API__LEVEL__());
   printf("amboso v%s\n",get_ANVIL__VERSION__());
   printf("New:%s\n",get_ANVIL__VERSION__DESC__());
   printf("Date:%s\n",get_ANVIL__VERSION__DATE__());
