1cdd1a5229ae7243c28ff70a5145d87af642e073
jgabaut <109908086+jgabaut@users.noreply.github.com>
2023-08-23 12:33:37 +0200

    feat: 1.6.3 better -G gen

--- example-src/hello_world.c
+++ example-src/hello_world.c
@@ -6,8 +6,10 @@ int main(void) {
   printf("Hello, World!\n");
   printf("Built using amboso API v%s\n",get_ANVIL__API__LEVEL__());
   printf("amboso v%s\n",get_ANVIL__VERSION__());
+  /*
   printf("New:%s\n",get_ANVIL__VERSION__DESC__());
   printf("Date:%s\n",get_ANVIL__VERSION__DATE__());
   printf("Author:%s\n",get_ANVIL__VERSION__AUTHOR__());
+  */
   return 0;
 }
