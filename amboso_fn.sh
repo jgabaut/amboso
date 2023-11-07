99e5afaf328c36cafc5d941643b87d1175343db8
jgabaut <109908086+jgabaut@users.noreply.github.com>
2023-11-07 04:36:32 +0100

    chore: set interpreter for amboso_fn.sh, add copyright

--- amboso_fn.sh
+++ amboso_fn.sh
@@ -1,3 +1,19 @@
+#!/bin/bash
+#  Bash symbols sourced by amboso.
+#    Copyright (C) 2023  jgabaut
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, version 3 of the License.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 AMBOSO_API_LVL="1.6.12"
 at () {
     echo -n "{ call: [$(( ${#BASH_LINENO[@]} - 1 ))] "
