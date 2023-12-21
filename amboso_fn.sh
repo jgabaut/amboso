b738da7f88fadccd033484424fe71966aab88e5d
jgabaut <109908086+jgabaut@users.noreply.github.com>
2023-12-22 00:31:42 +0100

    chore: less warning spam

--- amboso_fn.sh
+++ amboso_fn.sh
@@ -1261,7 +1261,7 @@ amboso_parse_args() {
   tot_opts=$OPTIND
   done
 
-  if [[ $quiet_flag -eq 0 && "${AMBOSO_LVL_REC}" -lt 3 ]]; then {
+  if [[ $quiet_flag -eq 0 && "${AMBOSO_LVL_REC}" -lt 2 ]]; then {
     printf "amboso, version $amboso_currvers\nCopyright (C) 2023  jgabaut\n\n  This program comes with ABSOLUTELY NO WARRANTY; for details type \`$(basename "$prog_name") -W\`.\n  This is free software, and you are welcome to redistribute it\n  under certain conditions; see file \`LICENSE\` for details.\n\n  Full source is available at https://github.com/jgabaut/amboso\n\n"
   }
   fi
