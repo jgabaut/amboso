009f5b3d53265af54b149667af7ad14378548902
jgabaut <109908086+jgabaut@users.noreply.github.com>
2024-08-28 18:12:37 +0200

    chore: update source test

--- example-src/hello_world.c
+++ example-src/hello_world.c
@@ -7,7 +7,7 @@ int main(void) {
   printf("Built using amboso API v%s\n",get_ANVIL__API__LEVEL__());
   printf("amboso v%s\n",get_ANVIL__VERSION__());
 
-  printf("Header gen time: {%s}\n", get_ANVIL__HEADER__GENTIME__()); // Only available from amboso >= 2.0.4
+  (void) get_ANVIL__HEADER__GENTIME__(); // Only available from amboso >= 2.0.4
   /*
   printf("New:%s\n",get_ANVIL__VERSION__DESC__());
   printf("Date:%s\n",get_ANVIL__VERSION__DATE__());
