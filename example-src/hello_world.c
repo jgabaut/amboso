ae3194e6a8900fdb71da67e1e320c3d156e85653
jgabaut <109908086+jgabaut@users.noreply.github.com>
2024-08-28 15:28:01 +0200

    chore: use header gentime in example source

--- example-src/hello_world.c
+++ example-src/hello_world.c
@@ -6,6 +6,8 @@ int main(void) {
   printf("Hello, World!\n");
   printf("Built using amboso API v%s\n",get_ANVIL__API__LEVEL__());
   printf("amboso v%s\n",get_ANVIL__VERSION__());
+
+  printf("Header gen time: {%s}\n", get_ANVIL__HEADER__GENTIME__()); // Only available from amboso >= 2.0.4
   /*
   printf("New:%s\n",get_ANVIL__VERSION__DESC__());
   printf("Date:%s\n",get_ANVIL__VERSION__DATE__());
